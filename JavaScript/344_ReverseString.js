c43602d0d29b94dda14e18e1a9b33e304d127b85
ting-chien <eric821210@gmail.com>
2021-06-13 16:21:21 +0800

    調整資料夾命名方式

--- JavaScript/344_ReverseString.js
+++ JavaScript/344_ReverseString.js
@@ -4,16 +4,15 @@
  */
 var reverseString = function(s) {
   // Method1. Naive
-  // for (let i=0; i<=s.length; i++) {
-  //   let temp = s.pop();
-  //   s.splice(i, 0, temp);
-  // }
+  for (let i=0; i<=s.length; i++) {
+    let temp = s.pop();
+    s.splice(i, 0, temp);
+  }
 
   // Method2. Half-loop
   for (let i=0; i<s.length/2; i++) {
     [s[i], s[s.length-i-1]] = [s[s.length-i-1], s[i]];
   }
-  console.log(s);
 };
 
 reverseString(["h","e","l","l","o"])
