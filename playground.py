cbf94cb0c97d8000ae4dd3e809fa49a0bf031d83
Eric Wang <eric.wang@theomnieyes.com>
2024-05-19 11:34:15 +0800

    78. Subsets

--- playground.py
+++ playground.py
@@ -9,25 +9,28 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        """透過迭代的方式找尋 binary tree 的 paths，和一般
-        作法不同的地方是，stack 除了會儲存 node 外，還會保留
-        當時的路徑紀錄。
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """此題希望透過 backtrack 的方式往下查找剩餘的 nums，
+        並將從 nums 裡面取出的元組組成一個陣列 path，在每一次遞
+        迴時傳給 res 作為一個 subset。
         """
-        if not root: return
-        stack = [(root, str(root.val))]
         res = []
-        while stack:
-            node, path = stack.pop()
-            # insert path into res if lead node
-            if not node.left and not node.right:
-                res.append(path)
-            # go throught left and right sub tree
-            if right := node.right:
-                stack.append((right, path + "->" + str(right.val)))
-            if left := node.left:
-                stack.append((left, path + "->" + str(left.val)))
-        return res
+        def backtrack(remains: List[int], path: List[int]):
+            print(f"remains={remains}, path={path}")
+            if remains == [1,2,3] and len(path) > 3: raise
+            nonlocal res
+            # append every subset into res
+            res.append(path[:])
+            print(f"res={res}")
+            # go through all elements in nums recursivelly
+            for i in range(len(remains)):
+                path.append(remains[i])
+                backtrack(remains[i+1:], path)
+                path.pop()
+            # return if there is no element left in nums
+            return
+        backtrack(nums, [])
+        return res        
     
 
 def traverse(root: Optional[TreeNode]) -> List[Any]:
@@ -39,21 +42,17 @@ def traverse(root: Optional[TreeNode]) -> List[Any]:
         res = res + traverse(root.right)
     return res
 
-root = TreeNode(3)
-root.left = TreeNode(9)
-root.right = TreeNode(20)
-root.right.left = TreeNode(15)
-root.right.right = TreeNode(7)
-print(traverse(root))
+# root = TreeNode(3)
+# root.left = TreeNode(9)
+# root.right = TreeNode(20)
+# root.right.left = TreeNode(15)
+# root.right.right = TreeNode(7)
+# print(traverse(root))
 
-# Test 1: root = [4,2,7,1,3], val = 2
-res = Solution().levelOrder(root=root)
+# Example 1: [1,2,3]
+res = Solution().subsets([1,2,3])
 print(res)
 
-# Test 2: root = [4,2,7,1,3], val = 5
-res = Solution().levelOrder(root=None)
-print(res)
-
-# Test 3: root = 1
-res = Solution().levelOrder(root=TreeNode(1))
+# Example 2: [0]
+res = Solution().subsets([0])
 print(res)
