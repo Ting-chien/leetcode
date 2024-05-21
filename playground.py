2dc3ab6d730369576c7cc51001518917abe9dc6b
Eric Wang <eric.wang@theomnieyes.com>
2024-05-21 22:21:40 +0800

    257. Binary Tree Paths

--- playground.py
+++ playground.py
@@ -9,28 +9,21 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        """此題希望透過 backtrack 的方式往下查找剩餘的 nums，
-        並將從 nums 裡面取出的元組組成一個陣列 path，在每一次遞
-        迴時傳給 res 作為一個 subset。
-        """
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         res = []
-        def backtrack(remains: List[int], path: List[int]):
-            print(f"remains={remains}, path={path}")
-            if remains == [1,2,3] and len(path) > 3: raise
+        def backtrack(node: Optional[TreeNode], path: str):
             nonlocal res
-            # append every subset into res
-            res.append(path[:])
-            print(f"res={res}")
-            # go through all elements in nums recursivelly
-            for i in range(len(remains)):
-                path.append(remains[i])
-                backtrack(remains[i+1:], path)
-                path.pop()
-            # return if there is no element left in nums
-            return
-        backtrack(nums, [])
-        return res        
+            # return case (if leaf node)
+            if not node.left and not node.right:
+                res.append(path)
+                return
+            # general case
+            if left := node.left:
+                backtrack(left, path + "->" + str(left.val))
+            if right := node.right:
+                backtrack(right, path + "->" + str(right.val))
+        backtrack(root, str(root.val)) # at least one node in tree
+        return res
     
 
 def traverse(root: Optional[TreeNode]) -> List[Any]:
@@ -42,17 +35,16 @@ def traverse(root: Optional[TreeNode]) -> List[Any]:
         res = res + traverse(root.right)
     return res
 
-# root = TreeNode(3)
-# root.left = TreeNode(9)
-# root.right = TreeNode(20)
-# root.right.left = TreeNode(15)
-# root.right.right = TreeNode(7)
-# print(traverse(root))
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(5)
+print(traverse(root))
 
 # Example 1: [1,2,3]
-res = Solution().subsets([1,2,3])
+res = Solution().binaryTreePaths(root)
 print(res)
 
 # Example 2: [0]
-res = Solution().subsets([0])
+res = Solution().binaryTreePaths(TreeNode(1))
 print(res)
