98371548073d050940abf591c10283c0be26abb4
Eric Wang <eric.wang@theomnieyes.com>
2024-05-30 23:58:17 +0800

    2. Add Two Numbers

--- playground.py
+++ playground.py
@@ -1,6 +1,26 @@
 from typing import Optional, List, Any
 
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def insert_nodes(arr: List[Any]) -> Optional[ListNode]:
+    dummy = curr = ListNode(0)
+    for ele in arr:
+        curr.next = ListNode(ele)
+        curr = curr.next
+    return dummy.next
+
+def print_nodes(node: Optional[ListNode]) -> List[Any]:
+    res = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    return res
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -43,18 +63,66 @@ def insert_level_order(arr: List[Any]) -> Optional[TreeNode]:
 
 
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        """使用 stack 解決問題，在 stack 中同時儲存 (node, path, curr_sum)"""
-        if not root: return 
-        res = []
-        stack = [(root, [root.val], root.val)]
-        while stack:
-            node, path, tmp = stack.pop()
-            if not node.left and not node.right and tmp == targetSum:
-                res.append(path[:])
-            if left := node.left:
-                stack.append((left, path+[left.val], tmp+left.val))
-            if right := node.right:
-                stack.append((right, path+[right.val], tmp+right.val))
-        return res
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        題目敘述：
+        1. 有兩條 linked-list 用來表示兩個非負整數
+        2. 數字的顯示方式是倒著排列
 
+        解題思路：
+
+        if I have l1 = [2, 4, 3], l2 = [5, 6, 4]
+        means 342 + 465 = 807
+        hence result is [7, 0, 8]
+
+        so, we start with the first digit
+            2 -> 4 -> 3
+          + 5 -> 6 -> 4
+        ----------------
+          = 7 -> 0 -> 8  
+        """
+        # set the result linked list
+        dummy = curr = ListNode(0)
+        tmp = 0
+        # go through l1 or l2 if node still exist
+        while tmp or l1 or l2:
+            # check if l1 can be calculated
+            if l1:
+                tmp += l1.val
+                l1 = l1.next
+            # check if l2 can be calculated
+            if l2:
+                tmp += l2.val
+                l2 = l2.next
+            # get quotient and remainder
+            div, mod = divmod(tmp, 10)
+            tmp = div
+            curr.next = ListNode(mod)
+            curr = curr.next
+        
+        return dummy.next
+    
+
+# Test 1
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Expect Output: [7,0,8]
+l1 = insert_nodes([2,4,3])
+l2 = insert_nodes([5,6,4])
+res = Solution().addTwoNumbers(l1, l2)
+print(print_nodes(res))
+
+# Test 2
+# Input: l1 = [0], l2 = [0]
+# Expect Output: [0]
+l1 = insert_nodes([0])
+l2 = insert_nodes([0])
+res = Solution().addTwoNumbers(l1, l2)
+print(print_nodes(res))
+
+# Test 3
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Expect Output: [8,9,9,9,0,0,0,1]
+l1 = insert_nodes([9,9,9,9,9,9,9])
+l2 = insert_nodes([9,9,9,9])
+res = Solution().addTwoNumbers(l1, l2)
+print(print_nodes(res))
