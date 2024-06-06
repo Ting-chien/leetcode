9b607eac6540477bcd8755914a03326c5463f772
Eric Wang <eric.wang@theomnieyes.com>
2024-06-06 23:42:18 +0800

    746. Min Cost Climbing Stairs

--- playground.py
+++ playground.py
@@ -65,14 +65,14 @@ def insert_level_order(arr: List[Any]) -> Optional[TreeNode]:
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         """Solution 1. DP(bottom-up)"""
-        # dp = [None]*(len(cost)+1)
-        # dp[0], dp[1] = 0, 0
-        # for i in range(2, len(cost)+1):
-        #     dp[i] = min(
-        #         dp[i-1] + cost[i-1],
-        #         dp[i-2] + cost[i-2]
-        #     )
-        # return dp[-1]
+        dp = [None]*(len(cost)+1)
+        dp[0], dp[1] = 0, 0
+        for i in range(2, len(cost)+1):
+            dp[i] = min(
+                dp[i-1] + cost[i-1],
+                dp[i-2] + cost[i-2]
+            )
+        return dp[-1]
         """Solution 2. Optimize space usage from solution 1"""
         a, b = 0, 0
         for i in range(2, len(cost)+1):
