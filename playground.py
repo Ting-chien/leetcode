20d47946f2d4321e968ba44531221f892359bfae
Eric Wang <eric.wang@theomnieyes.com>
2024-07-20 11:29:27 +0800

    tmp update

--- playground.py
+++ playground.py
@@ -63,66 +63,40 @@ def insert_level_order(arr: List[Any]) -> Optional[TreeNode]:
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def isPalindrome(self, s: str) -> bool:
         """
-        Finding three num [nums[i], nums[j], nums[k]] which
-        i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
-        Notice: There might be duplicate number in the array,
-        but no duplicate triplets is allowable.
-
-        Solution: 透過一個 pointer 來遍歷所有 array 中的元素，並且透過 two 
-        pointers 的解題方式找尋剩下 array 中的數字。
+        解題路線：
+        1. 移除非英數字的部分
+        2. 將所有字符轉成小寫
+        3. 透過左右兩隻指針，檢查是否所有遍歷過程中遇到的字符都一樣
         """
-        res = []
-        # 將 array 由小到大排序
-        nums.sort()
-        for i in range(len(nums)-2):
-            # 跳過與上一個相同的 num，來避免求出重複的答案
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            # 透過 two pointer 解出剩下的題目
-            target = 0 - nums[i]
-            j, k = i+1, len(nums)-1
-            while j < k:
-                print(j, k)
-                if nums[j] + nums[k] == target:
-                    res.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    while nums[j] == nums[j-1]: 
-                        if j < k:
-                            j += 1
-                        else:
-                            break
-                    while nums[k] == nums[k+1]: 
-                        if j < k:
-                            k -= 1
-                        else:
-                            break
-                elif nums[j] + nums[k] > target:
-                    k -= 1
-                else:
-                    j += 1
-        return res
+        # remove non-alphanumeric and turn string into lowercase
+        s = "".join([char for char in s if char.isalnum()])
+        s = s.lower()
+
+        # iterate s by left and right pointer, and check if s
+        # is a palindrome
+        l_p = 0
+        r_p = len(s)-1
+        while l_p < r_p:
+            if s[l_p] != s[r_p]: return False
+            l_p += 1
+            r_p -= 1
+
+        return True
 
 
 # Example 1:
-# Input: nums = [-1,0,1,2,-1,-4]
-# Output: [[-1,-1,2],[-1,0,1]]
-print(Solution().threeSum(nums=[-1,0,1,2,-1,-4]))
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+print(Solution().isPalindrome(s="A man, a plan, a canal: Panama"))
 
 # Example 2:
-# Input: nums = [0,1,1]
-# Output: []
-print(Solution().threeSum(nums=[0,1,1]))
+# Input: s = "race a car"
+# Output: false
+print(Solution().isPalindrome(s="race a car"))
 
 # Example 3:
-# Input: nums = [0,0,0]
-# Output: [[0,0,0]]
-print(Solution().threeSum(nums=[0,0,0]))
-
-# Example 3:
-# Input: nums = [-2,0,0,2,2]
-# Output: [[-2,0,2]]
-print(Solution().threeSum(nums=[-2,0,0,2,2]))
+# Input: s = " "
+# Output: true
+print(Solution().isPalindrome(s=" "))
