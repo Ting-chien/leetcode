540576c4f54aa5b51387e6b1a7dec54ccdb5f2ac
Eric Wang <eric.wang@theomnieyes.com>
2025-08-05 18:07:14 +0800

    LC 75: Day 17

--- Python/649_predictPartyVictory.py
+++ Python/649_predictPartyVictory.py
@@ -19,6 +19,41 @@ class Solution:
             senators.append(s)
 
         return "Radiant" if set(senators).pop() == "R" else "Dire"
+    
+
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        """
+        Use deque do handle append and insert with both O(1) in
+        time complexity.
+
+        * append(): O(1)
+        * appendleft(): O(1)
+        * pop(): O(1)
+        * popleft(): O(1)
+        """
+        rad = deque()
+        dir = deque()
+        N = len(senate)
+        # Put all senators' position to deque
+        for i, s in enumerate(senate):
+            if s == "R":
+                rad.append(i)
+            else:
+                dir.append(i)
+        # Compete two senators' position until one party take over
+        while rad and dir:
+            r = rad.popleft()
+            d = dir.popleft()
+            # Compare who win, senator with lower index win
+            if r < d:
+                rad.append(r+N)
+            else:
+                dir.append(d+N)
+
+        return "Radiant" if rad else "Dire"
+
 
 # Example 1:
 # Input: senate = "RD"
