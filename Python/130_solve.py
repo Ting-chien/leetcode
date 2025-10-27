65b91bd1c11f25a30ae4369aaddb76e487dc8f46
Eric Wang <eric.wang@theomnieyes.com>
2025-10-27 16:28:32 +0800

    Neetcode 150 Day 9

--- Python/130_solve.py
+++ Python/130_solve.py
@@ -102,6 +102,61 @@ class Solution:
                     board[i][j] = "X"
 
 
+class Solution:
+
+    parents = []
+
+    def find(self, x: int) -> int:
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x: int, y: int):
+        px, py = self.find(x), self.find(y)
+        if px != py:
+            self.parents[py] = px
+
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        透過 Union Find 算法，將為 "O" 的 cells 合併，若 region 內
+        有一 cell 位於邊界，則給定一 dummy head 作為 parent，要注意
+        Union Find 是將小樹掛到大樹上，因此 dummy head 的值需要大於
+        所有的 cells。
+        """
+        M, N = len(board), len(board[0])
+        self.parents = list(range(M*N+1))
+        dummy = M * N
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def get_id(i: int, j: int):
+            """
+            透過 get_id() 來為 board 上的 cells 賦予一個編號
+            """
+            return i*N + j
+
+        # 把 board 跑一遍，並將 board[i][j] == "O" 的部分合併
+        for i in range(M):
+            for j in range(N):
+                if board[i][j] == "O":
+                    if i == 0 or i == M-1 or j == 0 or j == N-1:
+                        # 如果 board[i][j] 在邊界，則將其 parent 設為 dummy head
+                        self.union(get_id(i, j), dummy)
+                    else:
+                        for dx, dy in directions:
+                            nx, ny = i + dx, j + dy
+                            if 0 <= nx < M and 0 <= ny < N and board[nx][ny] == "O":
+                                self.union(get_id(i, j), get_id(nx, ny))
+
+        # 將 baord 所有的 cells 檢查一遍，如果 board[i][j] == "O" && 源頭不為 dummy，則
+        # 則需要改為 "X"
+        for i in range(M):
+            for j in range(N):
+                if board[i][j] == "O":
+                    if self.find(get_id(i, j)) != self.find(dummy):
+                        board[i][j] = "X"
+
+        print(board)
+
 # Example 1:
 # Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 # Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
@@ -116,3 +171,8 @@ Solution().solve(board = [["X"]])
 # Input: board = [["O","O","O"],["O","O","O"],["O","O","O"]]]
 # Output: [["O","O","O"],["O","O","O"],["O","O","O"]]]
 Solution().solve(board = [["O","O","O"],["O","O","O"],["O","O","O"]])
+
+# Example 4:
+# Input: board = [["X","O","X","X"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"]]
+# Output: [["X","O","X","X"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"]]
+Solution().solve(board = [["X","O","X","X"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"],["X","O","X","O"],["O","X","O","X"]])
