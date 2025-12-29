f3bf107e2d21ee949ceb2267dc49407b658c0503
Eric Wang <eric.wang@theomnieyes.com>
2025-12-29 17:59:19 +0800

    155. Min Stack

---
+++ Leetcode/155. Min Stack/solution.py
@@ -0,0 +1,34 @@
+class MinStack:
+    """
+    利用 List.pop() 的時間複雜度為 O(1) 的特性，分別儲存 stack
+    和 min_stack 兩個 stack，以按照先後、大小順序儲存。
+    """
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
