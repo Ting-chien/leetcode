567d203596d19a1ccecec180ed69bfb9bd0c3397
Eric Wang <eric.wang@theomnieyes.com>
2026-06-15 11:23:24 +0800

    49. Group Anagrams

--- problems/0049_group_anagrams/solution.py
+++ problems/0049_group_anagrams/solution.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import defaultdict
 
 class Solution1:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -11,6 +12,21 @@ class Solution1:
             else:
                 d[sorted_str] = [str]
         return list(d.values())
+
+class Solution1:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagrams = defaultdict(list)
+        for s in strs:
+            # Get the key of str
+            count = [0] * 26
+            for ch in s:
+                count[ord(ch) - ord("a")] += 1
+            # Group the str
+            anagrams[tuple(count)].append(s)
+
+        return list(anagrams.values())
+
 
 if __name__ == '__main__':
     sol = Solution1()
